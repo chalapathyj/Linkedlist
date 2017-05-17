14b09353f3deea6a25e7b5fc11306c1d2bbf87d5
chalapathyj <chala_shank@yahoo.co.in>
2017-05-17 18:54:33 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,121 +1,58 @@
-class Node(object):
- 
-    def __init__(self, data, next):
+class Node:
+
+    def __init__(self, data, prev=None, nxt=None):
         self.data = data
-        self.next = next
- 
- 
-class SingleList(object):
- 
+        self.prev = prev
+        self.nxt = nxt
+
+
+class List:
     head = None
     tail = None
- 
-    def show(self):
-        print ("Showing list data:",  self.head.__dict__)
-        current_node = self.head
-        while current_node is not None:
-            print (current_node.data, " -> ",end = ' ')
-            current_node = current_node.next
-        print (None)
- 
-    def append(self, data):
-        node = Node(data, None)
-        if self.head is None:
-            self.head = self.tail = node
+    noOfNodes = 0
+
+    def _make_node(self, data):
+        try:
+            node = Node(data)
+            if self.head is None:
+                self.head = self.tail = node
+
+            List.noOfNodes += 1
+            return node
+        except:
+            raise "Failed to create the node"
+
+    def insert_after(self, data, ptr=None):
+        if ptr is None:
+            print("Calling make_node")
+            self._make_node(data)
         else:
-            self.tail.next = node
-        self.tail = node
- 
-    def remove(self, node_value):
-        current_node = self.head
-        previous_node = None
-        while current_node is not None:
-            if current_node.data == node_value:
-                # if this is the first node (head)
-                if previous_node is not None:
-                    previous_node.next = current_node.next
-                else:
-                    self.head = current_node.next
- 
-            # needed for the next iteration
-            previous_node = current_node
-            current_node = current_node.next
- 
- 
-s = SingleList()
-s.append(31)
-s.append(2)
-s.append(3)
-s.append(4)
- 
-s.show()
-s.remove(31)
-s.remove(3)
-s.remove(2)
-s.show()
+            node = self._make_node(data)
+            cur_node = self.head
+            while cur_node is not None:
+                if cur_node.data == ptr:
+                    node.nxt = cur_node.nxt
+                    cur_node.prev = node
+                    cur_node.nxt = node
+                    node.prev = cur_node
+                    print("Node")
+                    break
+                cur_node = cur_node.nxt
+
+    def show(self):
+        cur_node = self.head
+        while cur_node is not None:
+            print("showing node :", cur_node.data)
+            cur_node = cur_node.nxt
 
 
-# class Noclass Node(object):
- 
-#     def __init__(self, data, prev, next):
-#         self.data = data
-#         self.prev = prev
-#         self.next = next
- 
- 
-# class DoubleList(object):
- 
-#     head = None
-#     tail = None
- 
-#     def append(self, data):
-#         new_node = Node(data, None, None)
-#         if self.head is None:
-#             self.head = self.tail = new_node
-#         else:
-#             new_node.prev = self.tail
-#             new_node.next = None
-#             self.tail.next = new_node
-#             self.tail = new_node
- 
-#     def remove(self, node_value):
-#         current_node = self.head
- 
-#         while current_node is not None:
-#             if current_node.data == node_value:
-#                 # if it's not the first element
-#                 if current_node.prev is not None:
-#                     current_node.prev.next = current_node.next
-#                     current_node.next.prev = current_node.prev
-#                 else:
-#                     # otherwise we have no prev (it's None), head is the next one, and prev becomes None
-#                     self.head = current_node.next
-#                     current_node.next.prev = None
- 
-#             current_node = current_node.next
- 
-#     def show(self):
-#         print "Show list data:"
-#         current_node = self.head
-#         while current_node is not None:
-#             print current_node.prev.data if hasattr(current_node.prev, "data") else None,
-#             print current_node.data,
-#             print current_node.next.data if hasattr(current_node.next, "data") else None
- 
-#             current_node = current_node.next
-#         print "*"*50
- 
- 
-# d = DoubleList()
- 
-# d.append(5)
-# d.append(6)
-# d.append(50)
-# d.append(30)
- 
-# d.show()
- 
-# d.remove(50)
-# d.remove(5)
- 
-# d.show()
+newnde = List()
+newnde.insert_after(4)
+newnde.show()
+print(newnde.noOfNodes)
+newnde.insert_after(5, 4)
+newnde.insert_after(6, 4)
+newnde.insert_after(3, 6)
+newnde.insert_after(1, 5)
+newnde.show()
+print("No of nodes: ", newnde.noOfNodes)
